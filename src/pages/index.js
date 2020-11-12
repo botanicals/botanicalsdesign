43250fa2a9079d9ec1cff711429daaed7d1fce16
Talon Durrant <talonmdurrant@gmail.com>
2020-11-12 02:04:37 -0700

    ignoring images for now, added empty post md files

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,117 @@ export default function Home() {
     >
       <Section sectionHeading="We create beautiful plantscapes">
         <Row>
-          <Column width={50}>hello</Column>
-          <Column width={50}>hello</Column>
+          <Column width={50}>
+            <p>
+              Botanicals is a family-owned business operating out of St George,
+              Utah. We offer custom plantscape{" "}
+              <a href="https://botanicalsdesign.com/services/design/">
+                designs and installations
+              </a>
+              , as well as{" "}
+              <a href="https://botanicalsdesign.com/services/maintenance/">
+                plant maintenance accounts{" "}
+              </a>
+              to care for your plants.{" "}
+              <a href="https://botanicalsdesign.com/request-consultation/">
+                Request a free consultation today
+              </a>{" "}
+              to start designing your commercial or residential spaces.
+            </p>
+
+            <p>
+              At this time, our services are in Southern Utah only, but we are
+              also working to the trade only in Northern Utah for design
+              professionals.
+            </p>
+
+            <p>
+              We love everything plantscapes. We grew up at Sandia Farms, a
+              greenhouse owned by our father in St George, Utah. There we
+              developed a love for plants and helping other receive the positive
+              effects of having their space plantscaped.
+            </p>
+
+            <p>
+              In the last few years, we have also expanded our business to own
+              three retail locations where we sell ceramic pottery, metal art,
+              yard decor, and more. We have one{" "}
+              <a href="https://botanicalsdesign.com/locations/saint-george/">
+                location in St George, Utah
+              </a>
+              ,{" "}
+              <a href="https://botanicalsdesign.com/locations/sandy/">
+                another further north in Sandy, Utah
+              </a>
+              , and{" "}
+              <a href="https://botanicalsdesign.com/locations/kaysville/">
+                another even further north in Kaysville, Utah
+              </a>
+              .
+            </p>
+            <button>Request a free consultation today</button>
+          </Column>
+          <Column width={50}>
+            <figure>
+              <img
+                src="/images/index/botanicals-owners-nicole-braden-hancock.jpg"
+                alt="Botanicals owners and siblings Braden and Niki Hancock."
+              />
+              <figcaption>
+                Botanicals owners and siblings Braden and Niki Hancock.
+              </figcaption>
+            </figure>
+          </Column>
         </Row>
+      </Section>
+      <Section sectionHeading="We love our clients">
         <Row>
-          <Column width={25}>hello</Column>
-          <Column width={75}>hello</Column>
+          <Column width={50}>
+            <p>
+              Being a family business, we strive for one-on-one, personable
+              contact with our clients. We will work with you to customize
+              plantscapes and decor to fit all of your needs.{" "}
+            </p>
+
+            <p>
+              <a href="https://botanicalsdesign.com/request-consultation/">
+                Request a free consultation
+              </a>
+              , and we will work with you through the entire process of getting
+              your plantscapes planned, designed, and installed, relieving you
+              from the worry of hitting your deadline. At this time, our
+              services are in Southern Utah only, but we are also working to the
+              trade only in Northern Utah for design professionals.
+            </p>
+
+            <p>
+              We design for both commercial and residential spaces, indoor and
+              outdoor. We often work closely with Architects, Interior
+              Designers, and Landscapers.{" "}
+              <a href="https://botanicalsdesign.com/prospective-clients/">
+                Learn more about how we work with our clientele&#8230;
+              </a>
+            </p>
+          </Column>
+          <Column width={50}>
+            <figure>
+              <img
+                src="/images/index/botanicals-owners-nicole-braden-hancock.jpg"
+                alt="a custom s shaped planter botancials designed for ihc genomics"
+              />
+              <figcaption>
+                A custom S-shaped planter we designed for IHC Genomics
+              </figcaption>
+            </figure>
+          </Column>
+        </Row>
+      </Section>
+      <Section>
+        <Row>
+          <Column width={100}>hello</Column>
+          <Column width={33}>hello</Column>
+          <Column width={33}>hello</Column>
+          <Column width={33}>hello</Column>
         </Row>
         <Row>
           <Column width={25}>hello</Column>

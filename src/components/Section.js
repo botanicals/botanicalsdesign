aa6dd292a7c778fd7794390eb9300083718d1ccf
Talon Durrant <talonmdurrant@gmail.com>
2020-11-12 14:36:39 -0700

    removed bg color and fixed class

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,14 +3,10 @@ import React from "react"
 function Section(props) {
   return (
     <section
-      className={
-        props.fullWidth
-          ? "main__section main__section--full-width"
-          : "main__section"
-      }
+      className={props.fullWidth ? "section section--full-width" : "section"}
     >
       {props.fullWidth ? (
-        <div className="main__section__full-width-content-container">
+        <div className="section__full-width-content-container">
           <h2 class="heading heading__h2">{props.sectionHeading}</h2>
           {props.children}
         </div>

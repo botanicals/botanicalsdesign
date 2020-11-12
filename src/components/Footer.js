943969ca2ec031d37c2c32a8d7e20e8e8c0ebab8
Talon Durrant <talonmdurrant@gmail.com>
2020-11-12 00:08:42 -0700

    recreated the footer using the reuable components instead

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from "react"
 
+import Row from "../components/Row"
+import Column from "../components/Column"
+import Section from "../components/Section"
+
 function Footer() {
   return (
     <footer class="footer">
-      <div class="row u-change-height">
-        <img src="/assets/logos/botanicals-logo.png" class="footer__logo" />
-      </div>
-
-      <section class="footer__desktop-tablet">
-        <div class="row">
-          <div class="col-1-of-3">
+      <Section>
+        <Row>
+          <Column centered width={100}>
+            <img src="/assets/logos/botanicals-logo.png" class="footer__logo" />
+          </Column>
+        </Row>
+        <Row>
+          <Column width={33}>
             <h3 class="heading heading__h5 heading__h5--green">
               Visit Us at our Stores
             </h3>
@@ -39,27 +44,21 @@ function Footer() {
               href="/kaysville-contact"
               class="navigation__links navigation__links--footer"
             >
-              Send a Message
+              SEND A MESSAGE
             </a>
             <br />
             <a
               href="https://www.facebook.com/botanicalskaysville/"
               target="_blank"
             >
-              <img
-                src="/wp-content/uploads/2017/09/Asset-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/fb.png" class="footer__icon" />
             </a>
             <a
               href="https://www.instagram.com/botanicalsdesign/"
               target="_blank"
               class="footer__icon--leftmargin"
             >
-              <img
-                src="/wp-content/uploads/2019/04/instagram-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/ig.png" class="footer__icon" />
             </a>
 
             <li class="navigation__list">
@@ -88,24 +87,18 @@ function Footer() {
               href="/sandy-contact"
               class="navigation__links navigation__links--footer"
             >
-              Send a Message
+              SEND A MESSAGE
             </a>
             <br />
             <a href="https://www.facebook.com/BotanicalsSandy/" target="_blank">
-              <img
-                src="/wp-content/uploads/2017/09/Asset-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/fb.png" class="footer__icon" />
             </a>
             <a
               href="https://www.instagram.com/botanicalsdesign/"
               target="_blank"
               class="footer__icon--leftmargin"
             >
-              <img
-                src="/wp-content/uploads/2019/04/instagram-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/ig.png" class="footer__icon" />
             </a>
 
             <li class="navigation__list">
@@ -134,31 +127,24 @@ function Footer() {
               href="/stgeorge-contact"
               class="navigation__links navigation__links--footer"
             >
-              Send a Message
+              SEND A MESSAGE
             </a>
             <br />
             <a
               href="https://www.facebook.com/botanicalsstgeorge/"
               target="_blank"
             >
-              <img
-                src="/wp-content/uploads/2017/09/Asset-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/fb.png" class="footer__icon" />
             </a>
             <a
               href="https://www.instagram.com/botanicalsdesign/"
               target="_blank"
               class="footer__icon--leftmargin"
             >
-              <img
-                src="/wp-content/uploads/2019/04/instagram-1.png"
-                class="footer__icon"
-              />
+              <img src="/assets/icons/ig.png" class="footer__icon" />
             </a>
-          </div>
-
-          <div class="col-1-of-3">
+          </Column>
+          <Column width={33}>
             <h3 class="heading heading__h5 heading__h5--green">
               Stay up to Date
             </h3>
@@ -236,9 +222,8 @@ function Footer() {
                 <li>Careers</li>
               </a>
             </ul>
-          </div>
-
-          <div class="col-1-of-3">
+          </Column>
+          <Column width={33}>
             <h3 class="heading heading__h5 heading__h5--green">Work with Us</h3>
 
             <h4 class="heading heading__h6 heading__h6--green">
@@ -293,270 +278,24 @@ function Footer() {
                 <li>Request a Consultation</li>
               </a>
             </ul>
-          </div>
-        </div>
-      </section>
+          </Column>
+        </Row>
 
-      <section class="footer__mobile">
-        <div class="row">
-          <div class="col-1-of-1">
-            <h3 class="heading heading__h5 heading__h5--green">
-              Visit Us at one of our Stores
-            </h3>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-1-of-2">
-            <li class="navigation__list">
+        <Row>
+          <Column centered width={100}>
+            <span class="footer__copywrite-span">
+              &copy; 2020 BOTANICALS &bull; SITE BY{" "}
               <a
-                href="/sandy"
-                class="navigation__links navigation__links--footer"
+                href="http://talondurrant.com"
+                target="_blank"
+                class="footer__copywrite-link"
               >
-                <h4 class="heading heading__h6 heading__h6--green u-hover-link">
-                  Botanicals Sandy
-                </h4>
+                TALON DURRANT
               </a>
-            </li>
-            <p class="u-default-font-size">
-              8610 South State Street
-              <br />
-              Sandy, UT 84070
-              <br />
-              <a
-                href="tel:8015671255"
-                class="navigation__links navigation__links--footer"
-              >
-                801.567.1255
-              </a>
-              <a
-                href="/sandy-contact"
-                class="navigation__links navigation__links--footer"
-              >
-                Send a Message
-              </a>
-              <br />
-            </p>
-            <a href="https://www.facebook.com/BotanicalsSandy/" target="_blank">
-              <img
-                src="/wp-content/uploads/2017/09/Asset-1.png"
-                class="footer__icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/botanicalsdesign/"
-              target="_blank"
-              class="footer__icon--leftmargin"
-            >
-              <img
-                src="/wp-content/uploads/2019/04/instagram-1.png"
-                class="footer__icon"
-              />
-            </a>
-          </div>
-          <div class="col-1-of-2">
-            <li class="navigation__list">
-              <a
-                href="/saint-george"
-                class="navigation__links navigation__links--footer"
-              >
-                <h4 class="heading heading__h6 heading__h6--green u-hover-link">
-                  Botanicals St. George
-                </h4>
-              </a>
-            </li>
-            <p class="u-default-font-size">
-              592 North Bluff Street
-              <br />
-              St. George, UT 84770
-              <br />
-              <a
-                href="tel:4356562151"
-                class="navigation__links navigation__links--footer"
-              >
-                435.656.2151
-              </a>
-              <a
-                href="/stgeorge-contact"
-                class="navigation__links navigation__links--footer"
-              >
-                Send a Message
-              </a>
-              <br />
-            </p>
-            <a
-              href="https://www.facebook.com/Botanicals-219925108044912/"
-              target="_blank"
-            >
-              <img
-                src="/wp-content/uploads/2017/09/Asset-1.png"
-                class="footer__icon"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/botanicalsdesign/"
-              target="_blank"
-              class="footer__icon--leftmargin"
-            >
-              <img
-                src="/wp-content/uploads/2019/04/instagram-1.png"
-                class="footer__icon"
-              />
-            </a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-1-of-2">
-            <h3 class="heading heading__h5 heading__h5--green">
-              Stay up to Date
-            </h3>
-            <h4 class="heading heading__h6 heading__h6--green">
-              Explore Our Site
-            </h4>
-            <ul class="navigation__list">
-              <a
-                href="/why-plantscape"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Plantscapes</li>
-              </a>
-              <a
-                href="/services"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Services</li>
-              </a>
-              <a
-                href="/prospective-clients"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Prospective Clients</li>
-              </a>
-              <a
-                href="/portfolios"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Portfolios</li>
-              </a>
-              <a
-                href="/locations"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Locations</li>
-              </a>
-              <a
-                href="/blog"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Blog</li>
-              </a>
-              <a
-                href="/about"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>About</li>
-              </a>
-            </ul>
-            <h4 class="heading heading__h6 heading__h6--green">Learn More</h4>
-            <ul class="navigation__list">
-              <a
-                href="/announcements"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Announcements</li>
-              </a>
-              <a
-                href="/news-posts"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Botanicals News</li>
-              </a>
-              <a
-                href="/events"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Botanicals Events</li>
-              </a>
-              <a
-                href="/jobs"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Careers</li>
-              </a>
-            </ul>
-          </div>
-
-          <div class="col-1-of-2">
-            <h3 class="heading heading__h5 heading__h5--green">Work with Us</h3>
-
-            <h4 class="heading heading__h6 heading__h6--green">
-              Design Professionals
-            </h4>
-            <ul class="navigation__list">
-              <a
-                href="/prospective-clients#landscapers"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Landscapers</li>
-              </a>
-              <a
-                href="/prospective-clients#architects"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Architects</li>
-              </a>
-              <a
-                href="/prospective-clients#interior-designers"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Interior Designers</li>
-              </a>
-            </ul>
-            <h4 class="heading heading__h6 heading__h6--green">
-              Plantscape Designs
-            </h4>
-            <ul class="navigation__list">
-              <a
-                href="/prospective-clients#residential"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Residential Spaces</li>
-              </a>
-              <a
-                href="/prospective-clients#commercial"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Commercial Spaces</li>
-              </a>
-            </ul>
-            <h4 class="heading heading__h6 heading__h6--green">
-              Get a Free Consultation
-            </h4>
-            <ul class="navigation__list">
-              <a
-                href="/request-consultation"
-                class="navigation__links navigation__links--footer"
-              >
-                <li>Request a Consultation</li>
-              </a>
-            </ul>
-          </div>
-        </div>
-      </section>
-
-      <div class="row u-change-height">
-        <div class="col-1-of-1">
-          <span class="footer__copywrite-span">
-            &copy; 2020 BOTANICALS &bull; BY{" "}
-            <a
-              href="http://talondurrant.com"
-              target="_blank"
-              class="footer__copywrite-link"
-            >
-              TALON DURRANT
-            </a>
-          </span>
-        </div>
-      </div>
+            </span>
+          </Column>
+        </Row>
+      </Section>
 
       {/* <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117112939-1"></script>

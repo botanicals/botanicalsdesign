63f117802eea48ad6495668ffc1f852f823d8598
Talon Durrant <talonmdurrant@gmail.com>
2020-11-12 14:56:12 -0700

    created 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+import PageLayout from "../layouts/PageLayout"
+import Section from "../components/Section"
+
+export default function Home() {
+  return (
+    <PageLayout
+      heroImage="hero-image-placeholder.jpg"
+      mainHeading="Page not found"
+      subHeading=""
+      pageHeading="Sorry..."
+    >
+      <Section sectionHeading="We couldn't find the page you are looking for">
+        <p>
+          We are sorry, the page you are looking for could not be located. This
+          may be due to a broken link, expired bookmark, or mistyped URL. Please
+          try again using our menu above. We hope you find what you are looking
+          for!
+        </p>
+      </Section>
+    </PageLayout>
+  )
+}

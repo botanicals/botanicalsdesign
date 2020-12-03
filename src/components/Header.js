1cd45b6a574ec5b0fe1ab5406427d6a1f309a18b
Talon Durrant <talonmdurrant@gmail.com>
2020-12-02 19:22:12 -0700

    switched places of li and a, use gatsby link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,57 @@
 import React from "react"
+import { Link } from "gatsby"
 
 function Header() {
   return (
     <>
       <header className="header">
-        <a href="/" className="header__logo header__logo--desktop">
+        <Link to="/" className="header__logo header__logo--desktop">
           <img src="/assets/logos/botanicals-logo.png" alt="botanicals logo" />
-        </a>
-        <a href="/" className="header__logo header__logo--mobile">
+        </Link>
+        <Link to="/" className="header__logo header__logo--mobile">
           <img
             className="header__logo--mobile-image"
             src="/assets/logos/botanicals-logo-mobile.png"
             alt="botanicals logo"
           />
-        </a>
+        </Link>
         <div className="navigation">
           <ul className="navigation__list">
-            <a href="/why-plantscape" className="navigation__links">
-              <li>Why Plantscape?</li>
-            </a>
-            <a href="/services" className="navigation__links">
-              <li>Services</li>
-            </a>
-            <a href="/prospective-clients" className="navigation__links">
-              <li>Prospective Clients</li>
-            </a>
-            <a href="/portfolios" className="navigation__links">
-              <li>Portfolios</li>
-            </a>
-            <a href="/locations" className="navigation__links">
-              <li>Locations</li>
-            </a>
-            <a href="/blog" className="navigation__links">
-              <li>Blog</li>
-            </a>
-            <a href="/about" className="navigation__links">
-              <li>About</li>
-            </a>
+            <li>
+              <Link to="/why-plantscape" className="navigation__links">
+                Why Plantscape?
+              </Link>
+            </li>
+            <li>
+              <Link to="/services" className="navigation__links">
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="/prospective-clients" className="navigation__links">
+                Prospective Clients
+              </Link>
+            </li>
+            <li>
+              <Link to="/portfolios" className="navigation__links">
+                Portfolios
+              </Link>
+            </li>
+            <li>
+              <Link to="/locations" className="navigation__links">
+                Locations
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" className="navigation__links">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="navigation__links">
+                About
+              </Link>
+            </li>
           </ul>
         </div>
       </header>

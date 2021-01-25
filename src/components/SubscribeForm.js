c1b98ec3cac0e9788236c4f7e27eaab5707f697c
Talon Durrant <talonmdurrant@gmail.com>
2021-01-25 14:46:34 -0700

    version one of the subscribe form

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 function SubscribeForm(props) {
+  const FORM_ACTION = "https://app.convertkit.com/forms/1665028/subscriptions"
+
+  const [northern, setNorthern] = useState(true)
+  const [southern, setSouthern] = useState(true)
+  const [professional, setProfessional] = useState(true)
+  const [updates, setUpdates] = useState(true)
+
+  const handleCheck = name => {
+    switch (name) {
+      case "northern":
+        setNorthern(!northern)
+        break
+      case "southern":
+        setSouthern(!southern)
+        break
+      case "professional":
+        setProfessional(!professional)
+        break
+      case "updates":
+        setUpdates(!updates)
+        break
+      default:
+        break
+    }
+  }
+
   return (
     <form
       method="post"
-      action="/newsletter/success"
+      action={FORM_ACTION}
       netlify-honeypot="bot-field"
       data-netlify="true"
       name="subscribe"
@@ -32,15 +58,101 @@ function SubscribeForm(props) {
         </label>
         <label className="top">
           Name
-          <input type="text" name="name" required />
+          <input type="text" name="first_name" required />
         </label>
         <label className="top">
           Email
-          <input type="email" name="email" required />
+          <input type="email" name="email_address" required />
         </label>
 
         <div>I want to recieve emails with:</div>
-        <label className="sbs">
+
+        <fieldset
+          data-group="checkboxes"
+          class="formkit-6487"
+          type="Custom"
+          order="3"
+          save_as="Tag"
+          group="field"
+        >
+          <div
+            class="formkit-checkboxes"
+            data-element="tags-checkboxes"
+            data-group="checkbox"
+          >
+            <label for="tag-20298694-2131965" className="sbs">
+              <input
+                className="formkit-checkbox"
+                id="tag-20298694-2131965"
+                type="checkbox"
+                name="tags[]"
+                value="2131965"
+                checked={northern}
+                onClick={() => handleCheck("northern")}
+              />
+              Shipment Notifications (Northern Utah)
+            </label>
+          </div>
+
+          <div
+            class="formkit-checkboxes"
+            data-element="tags-checkboxes"
+            data-group="checkbox"
+          >
+            <label for="tag-20298694-2131966" className="sbs">
+              <input
+                className="formkit-checkbox"
+                id="tag-20298694-2131966"
+                type="checkbox"
+                name="tags[]"
+                value="2131966"
+                checked={southern}
+                onClick={() => handleCheck("southern")}
+              />
+              Shipment Notifications (Southern Utah)
+            </label>
+          </div>
+
+          <div
+            className="formkit-checkboxes"
+            data-element="tags-checkboxes"
+            data-group="checkbox"
+          >
+            <label for="tag-20298694-2131967" className="sbs">
+              <input
+                className="formkit-checkbox"
+                id="tag-20298694-2131967"
+                type="checkbox"
+                name="tags[]"
+                value="2131967"
+                checked={professional}
+                onClick={() => handleCheck("professional")}
+              />
+              Professional Tips &amp; Tricks
+            </label>
+          </div>
+
+          <div
+            className="formkit-checkboxes"
+            data-element="tags-checkboxes"
+            data-group="checkbox"
+          >
+            <label for="tag-20298694-2131968" className="sbs">
+              <input
+                className="formkit-checkbox"
+                id="tag-20298694-2131968"
+                type="checkbox"
+                name="tags[]"
+                value="2131968"
+                checked={updates}
+                onClick={() => handleCheck("updates")}
+              />
+              Updates, Events &amp; News
+            </label>
+          </div>
+        </fieldset>
+
+        {/* <label className="sbs">
           <input
             type="checkbox"
             name="shipment-updates-northern-utah"
@@ -65,7 +177,7 @@ function SubscribeForm(props) {
         <label className="sbs">
           <input type="checkbox" name="updates-and-news" value="yes" checked />{" "}
           Updates &amp; News
-        </label>
+        </label> */}
 
         <input className="submit" type="submit" value="Sign Me Up" />
       </fieldset>

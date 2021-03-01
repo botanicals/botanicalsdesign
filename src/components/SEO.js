8176f4c67ec255f1f0dbc289fff52811608fcb9e
Talon Durrant <talonmdurrant@gmail.com>
2021-03-01 15:38:17 -0700

    added trailing slash

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -40,7 +40,7 @@ const SEO = props => {
         return (
           <Helmet>
             <html lang="en"></html>
-            {urlPath ? <link rel="canonical" href={seo.url} /> : null}
+            {urlPath ? <link rel="canonical" href={`${seo.url}/`} /> : null}
             <title>{seo.title}</title>
             <meta name="description" content={seo.description} />
 

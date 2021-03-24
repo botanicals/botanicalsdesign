6cccc6257abddbfcd95e8df129a3f4433b52a201
Talon Durrant <talonmdurrant@gmail.com>
2021-03-24 17:07:28 -0600

    made some adjustments

--- src/pages/newsletter/please-confirm.js
+++ src/pages/newsletter/please-confirm.js
@@ -4,17 +4,20 @@ import "../../styles/main.scss"
 
 export default () => {
   return (
-    <div className="full-page" style={{ padding: "4rem 10rem" }}>
+    <div
+      className="full-page"
+      style={{ padding: "0 10rem", backgroundColor: "#F3F1F1" }}
+    >
       <p>
         Thank you for subscribing to our emailing list! We are excited to be in
         communication with you.
       </p>
       <p>
-        <strong>IMPORTANT: </strong> If this is your first time subscribing,
-        please visit your email to confirm your subscription. You will NOT
-        recieve emails until you have confirmed your subscription via email. If
-        you do not recieve a confirmation email, try checking your spam. The
-        email could have accidentally been sent there by your filters.
+        <strong>IMPORTANT: </strong> Please visit your email to confirm your
+        subscription. You will NOT recieve emails until you have confirmed your
+        subscription via email. If you do not recieve a confirmation email, try
+        checking your spam. The email could have accidentally been sent there by
+        your filters.
       </p>
     </div>
   )

2c4857da10b5f6933fba6c41a25fbbf570d567f9
Talon Durrant <talonmdurrant@gmail.com>
2021-03-24 16:54:19 -0600

    setting up a please confirm page

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -3,6 +3,7 @@ import React from "react"
 function SubscribeForm() {
   return (
     <iframe
+      title="AWeber_Subscription_Form"
       src="https://forms.aweber.com/form/34/668219434.htm"
       frameborder="0"
       width="100%"
